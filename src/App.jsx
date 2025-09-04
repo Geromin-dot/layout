cf1a150ac3deda02ba1fc6a82f797518b29f6726
Geromin-dot <grv_manese.student@ua.edu.ph>
2025-09-04 11:06:13 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="container">
+      <div className="header">
+        Header
+      </div>
+      <div className="content">
+        <div className="leftside">
+          <div className="hero">Hero</div>
+          <div className="sidebar">Sidebar</div>
+        </div>
+        <div className="rightside">
+          <div className="main">Main Content</div>
+          <div className="extra">Extra Content</div>
+        </div>
+      </div>
+      <div className="content2">
+        <div className="ri">Related Images</div>
+        <div className="rp">Related Posts</div>
+      </div>
+      <div className="footer">Footer</div>
+    </div>
+  )
+}
+
+export default App
